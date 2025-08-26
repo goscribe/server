b9bfaabe32696df2ce422c817a9644301e9a2905
HLCLJacksonC <caizl@vip.163.com>
2025-08-26 12:30:38 +0800

    chore: npm setup

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export type { AppRouter } from "./routers/_app";
+export type { RouterInputs } from "./routers/_app";
+export type { RouterOutputs } from "./routers/_app";
--- src/routers/_app.ts
+++ src/routers/_app.ts
@@ -1,3 +1,4 @@
+import { inferRouterInputs, inferRouterOutputs } from '@trpc/server';
 import { router } from '../trpc';
 import { auth } from './auth';
 import { workspace } from './workspace';
@@ -9,3 +10,5 @@ export const appRouter = router({
 
 // Export type for client inference
 export type AppRouter = typeof appRouter;
+export type RouterInputs = inferRouterInputs<AppRouter>;
+export type RouterOutputs = inferRouterOutputs<AppRouter>;
